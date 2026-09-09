8b1d10d50f0d37124de222ab17a6a72df19ec34e
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2026-09-09 18:10:44 -0500

    Ejercicio 3, taller 1

--- SRC/Ejercicio3/Estudiante.java
+++ SRC/Ejercicio3/Estudiante.java
@@ -1,4 +1,29 @@
 package Ejercicio3;
 
 public class Estudiante {
+    String nombre;
+    int edad;
+    String curso;
+
+    public Estudiante() {
+        this.nombre = "Sin nombre";
+        this.edad = 0;
+        this.curso = "Sin curso";
+    }
+
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.curso = "No asignado";
+    }
+
+    public Estudiante(String nombre, int edad, String curso) {
+        this(nombre, edad);
+        this.curso = curso;
+    }
+
+    public static void main(String[] args) {
+        Estudiante estudiante = new Estudiante("Emanuel", 17, "Ingeniería de Software");
+        System.out.println("Ejecutando sin problemas");
+    }
 }
