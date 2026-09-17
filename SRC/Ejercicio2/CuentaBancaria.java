a6fd7e1c5df89c127af3a79fb6da362815637bfb
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2026-09-16 23:18:41 -0500

    Ejercicio 2 listo

--- SRC/Ejercicio2/CuentaBancaria.java
+++ SRC/Ejercicio2/CuentaBancaria.java
@@ -1,32 +1,42 @@
 package Ejercicio2;
 
 public class CuentaBancaria {
-
     private String numeroCuenta;
     private double saldo;
     private String tipoCuenta;
 
+    // 1. Constructor por defecto
     public CuentaBancaria() {
         this.numeroCuenta = "Sin asignar";
         this.saldo = 0.0;
         this.tipoCuenta = "Sin asignar";
     }
 
+    // 2. Constructor con dos parámetros
     public CuentaBancaria(String numeroCuenta, String tipoCuenta) {
         this.numeroCuenta = numeroCuenta;
-        this.saldo = 0.0;
         this.tipoCuenta = tipoCuenta;
+        this.saldo = 0.0;
     }
 
+    // 3. Constructor con tres parámetros
     public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
         this.numeroCuenta = numeroCuenta;
         this.saldo = saldo;
         this.tipoCuenta = tipoCuenta;
     }
 
+    // Método para imprimir
+    public void mostrarInformacion() {
+        System.out.println("Información de la Cuenta");
+        System.out.println("Número de cuenta: " + this.numeroCuenta);
+        System.out.println("Tipo de cuenta: " + this.tipoCuenta);
+        System.out.println("Saldo actual: $" + this.saldo);
+    }
+
+    // Ejecución de prueba
     public static void main(String[] args) {
-        CuentaBancaria cuenta1 = new CuentaBancaria();
-        CuentaBancaria cuenta2 = new CuentaBancaria("10023456", "Ahorros");
-        CuentaBancaria cuenta3 = new CuentaBancaria("20098765", 500000.0, "Corriente");
+        CuentaBancaria cuenta1 = new CuentaBancaria("10023456", 150000.0, "Ahorros");
+        cuenta1.mostrarInformacion();
     }
 }
